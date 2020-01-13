0c19974c080f97e2acc6bdd7c5c1f9598544ac35
EdouardW <Dev@MBP-de-Edouard.localdomain>
2020-01-13 14:58:53 +0100

    essai

--- tf_keras.py
+++ tf_keras.py
@@ -4,9 +4,15 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+# Data
 data = keras.datasets.fashion_mnist
-
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
-print ("Dimensions train_images: {}, Dimensions train_labels: {}".format(len(train_images), len(train_labels)))
-print ("Dimensions test_images: {}, Dimensions test_labels: {}".format(len(test_images), len(test_labels)))
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+#print ("Dimensions train_images: {}, Dimensions train_labels: {}".format(len(train_images), len(train_labels)))
+#print ("Dimensions test_images: {}, Dimensions test_labels: {}".format(len(test_images), len(test_labels)))
+
+print (train_images[0].shape)
